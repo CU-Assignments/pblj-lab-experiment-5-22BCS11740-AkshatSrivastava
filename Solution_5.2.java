8f0dd257ff133e20ec98d097114e60bd648fabdc
22BCS11740-AkshatSrivastava <22BCS11740@cuchd.in>
2025-02-21 14:22:28 +0530

    Create Solution_5.2.java

---
+++ Solution_5.2.java
@@ -0,0 +1,60 @@
+import java.io.*;
+
+class Student implements Serializable {
+    private int id;
+    private String name;
+    private double gpa;
+
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public void displayStudent() {
+        System.out.println("Student ID: " + id + ", Name: " + name + ", GPA: " + gpa);
+    }
+}
+
+public class StudentSerialization {
+   
+    public static void serializeStudent(Student student, String filename) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(student);
+            System.out.println("Student object has been serialized and saved to file.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found.");
+        } catch (IOException e) {
+            System.out.println("Error: Unable to serialize object.");
+        }
+    }
+
+    public static Student deserializeStudent(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            System.out.println("Student object has been deserialized.");
+            return (Student) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found.");
+        } catch (IOException e) {
+            System.out.println("Error: Unable to deserialize object.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: Class not found.");
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String filename = "student.ser";
+
+        Student student1 = new Student(1, "John Doe", 3.75);
+        serializeStudent(student1, filename);
+
+        Student deserializedStudent = deserializeStudent(filename);
+        if (deserializedStudent != null) {
+            System.out.println("Deserialized Student Details:");
+            deserializedStudent.displayStudent();
+        }
+
+        deserializeStudent("nonexistent.ser");
+    }
+}
